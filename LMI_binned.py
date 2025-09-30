238d4e81756077a74536330bd2a9aaed246456e7
Julian Raic <54615206+julkostar@users.noreply.github.com>
2025-09-30 21:25:06 +1000

    Update LMI_binned.py

--- LMI_binned.py
+++ LMI_binned.py
@@ -1,4 +1,3 @@
-print("Running LMI.py")
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
@@ -22,7 +21,7 @@ stops = pd.read_csv("datasets/stops.csv", usecols=STOPS_COL)
 # initialises data with preprocessing functions
 result = quick_data()
 
-#Compute travel efficiency as the average of (distance / travel time) across all trips for each individual (persid).
+#Compute Wasted time during travel for work and school
 
 work_trip["wasted_time_work"] = pd.to_numeric(work_trip["journey_elapsed_time"], errors='coerce') - pd.to_numeric(work_trip["journey_travel_time"], errors='coerce')
 work_trip["distance_work"] = work_trip["journey_distance"]
@@ -34,9 +33,9 @@ education_trip["distance_education"] = education_trip["journey_distance"]
 education_trip["time_education"] = education_trip["journey_travel_time"]
 overall_education_time_waste = education_trip[["persid", "wasted_time_education", "distance_education","time_education"]].drop_duplicates()
 
-merged = pd.merge(overall_education_time_waste, overall_work_time_waste, on="persid", how="outer")
 
-# Create binning categories for modes of transport
+
+# Create binning categories for modes of transport, apply them
 mapping = {
     "Bicycle": "Active", "Mobility Scooter": "Active", "Motorcycle": "Private",
     "Public Bus": "Public", "Rideshare Service": "Public", "School Bus": "Public",
@@ -47,27 +46,16 @@ mapping = {
 
 result["most_used_mode"] = result["most_used_mode"].replace(mapping)
 # Pick work if it exists, otherwise education
+merged = pd.merge(overall_education_time_waste, overall_work_time_waste, on="persid", how="outer")
 merged["wasted_time"] = merged["wasted_time_work"].combine_first(merged["wasted_time_education"])
 merged["distance"] = merged["distance_work"].combine_first(merged["distance_education"])
 merged["time"] = merged["time_work"].combine_first(merged["time_education"])
-# Final tidy dataframe
+
+# tidy dataframe
 overall_time_waste = merged[["persid", "wasted_time", "distance","time"]]
-
-print("Shapes before merge:", result.shape, overall_education_time_waste.shape, overall_work_time_waste.shape, overall_time_waste.shape)
-
 result = result.merge(overall_time_waste, on='persid', how='left')
 
-
-print(result.columns)
-types_df = result.iloc[0].map(type)
-for col in result.columns:
-    print("Type of", col, ":", types_df[col])
-    print("Example data in", col, ":", result[col].dropna().values[:5])
-
-print("Data shape:", result.shape)
-for col in result.columns:
-    print("Nan in", col, ":", result[col].isna().sum())
-
+# Specify data to be used in analysis
 KNN_df = result[["agegroup", "overall_trip_efficiency", "wasted_time", "distance", "persinc", "totalwfh","time"]]
 
 # Scales required Data to prevent bias
@@ -81,7 +69,7 @@ X_train, X_test, y_train, y_test = train_test_split(norm_KNN_data.dropna(), resu
 knn = KNeighborsClassifier(n_neighbors=6)  # You can change the number of neighbors
 knn.fit(X_train, y_train)
 
-# Demonstrates accuracy of KNN model
+# Demonstrates accuracy of KNN model with classification report parameters
 y_pred = knn.predict(X_test)
 print("Accuracy:", accuracy_score(y_test, y_pred))
 print("Classification Report:\n", classification_report(y_test, y_pred))
@@ -94,7 +82,7 @@ plt.ylabel("Actual")
 plt.title("Confusion Matrix for KNN")
 plt.show()
 
-
+#Creates the graph to find the optimal K value
 error_rates = []
 
 for k in range(1, 21):
@@ -108,4 +96,5 @@ plt.plot(range(1, 21), error_rates, marker='o', linestyle='--', color='blue')
 plt.title('Error Rate vs. K Value')
 plt.xlabel('K')
 plt.ylabel('Error Rate')
+
 plt.show() 
